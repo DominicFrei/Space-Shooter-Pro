593caf170a9a654ec0bdd44b8907ea141e186569
Dominic Frei <business.dominic.frei@gmail.com>
2020-08-30 23:19:20 +0100

    Add a rotating astroid at the beginning of the game.

---
+++ Assets/Scripts/Astroid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    private readonly float _rotateSpeed = 3.0f;
+    
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, _rotateSpeed * Time.deltaTime));
+    }
+}
