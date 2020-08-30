59854e334085ba04072c445c00784521c2746aa2
Dominic Frei <business.dominic.frei@gmail.com>
2020-08-30 15:27:58 +0100

    Add restart functionality.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -11,6 +11,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Sprite[] _sprites = null;
     [SerializeField] private Image _lifes = null;
     [SerializeField] private Text _gameOverText = null;
+    private bool _isGameOver = false;
     private WaitForSeconds _gameOverFlickerDelay = new WaitForSeconds(0.25f);
 
     private void Start()
@@ -18,6 +19,18 @@ public class UIManager : MonoBehaviour
         _scoreText.text = "Score: 0";
         _highscoreText.text = "Highscore: " + PlayerPrefs.GetInt("highscore");
         _gameOverText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_isGameOver)
+        {
+            bool rPressed = Input.GetKeyDown(KeyCode.R);
+            if (rPressed)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+            }
+        }
     }
 
     public void UpdateScore(int score, int highscore)
@@ -38,6 +51,7 @@ public class UIManager : MonoBehaviour
             _lifes.sprite = _sprites[lives];
             if (0 == lives)
             {
+                _isGameOver = true;
                 StartCoroutine(ShowGameOver());
             }
         }
