457d4b7f5ea69adf6272d747bc97fb852e87455d
Dominic Frei <business.dominic.frei@gmail.com>
2020-08-31 01:31:09 +0100

    Add the explosion to the asteroid.

--- Assets/Scripts/Astroid.cs
+++ Assets/Scripts/Astroid.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Astroid : MonoBehaviour
 {
     private readonly float _rotateSpeed = 3.0f;
-    
+    [SerializeField] private GameObject _explosion = null;
 
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, _rotateSpeed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Laser"))
+        {
+            Destroy(collision.gameObject);
+            _ = Instantiate(_explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject, 0.1f);
+        }
     }
 }
--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void startGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("Game");
     }
